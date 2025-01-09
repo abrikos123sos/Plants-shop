117ffa2cbb743e38f52c82d0bfa8ba992817a63a
abrikos123sos <vlad.shepilka@mail.ru>
2025-01-09 22:56:06 +0300

    modified:   package-lock.json 	modified:   package.json 	new file:   public/img/korobka.png 	new file:   public/img/order.png 	new file:   public/img/svg/add.png 	new file:   public/img/svg/profile.svg 	modified:   src/App.css 	modified:   src/App.css.map 	modified:   src/App.js 	modified:   src/App.scss 	new file:   src/apjscontext.js 	modified:   src/cart.json 	modified:   src/components/BlockNewPlants/BlockNewPlants.jsx 	modified:   src/db.json 	new file:   src/drawerfavorite.json 	modified:   src/index.js 	new file:   src/orders.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
 import MainContainer from './components/Page/MainContainer/MainContainer';
-import {BrowserRouter, Routes, Route, Form} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Forms from './components/Form/Forms';
 import React from 'react';
 import ShopPage from './components/Page/ShopPage/ShopPage';
+
+
+
+
+import DrawerBtn from './components/DrawerBtn';
+import FavoriteDrawer from './components/BlockNewPlants/FavoriteDrawer';
+import MyContext from './apjscontext';
+
 
 /*https://dribbble.com/shots/22341969-Plant-shop-landing-page 
 ,
@@ -14,6 +22,7 @@ function App() {
 
 
 
+const {cartFavorite} = React.useContext(MyContext)
 const handleScrollTop = () =>{
 window.scrollTo({
   top:0,
@@ -23,18 +32,24 @@ window.scrollTo({
 }
 
   return (
+
     <div className="App">
+
       <div onClick={handleScrollTop} className='blockUpArrow'>
  <img className='arrowup' src='./img/svg/arrowUp.svg' />
       </div>
-      <BrowserRouter>
-      <Routes>
-<Route path ="/" element={<MainContainer/>}/>
+
+      
+<BrowserRouter>
+<Routes>
 <Route path ="/form" element={<Forms/>}/>
 <Route path ="/shop" element={<ShopPage/>}/>
+<Route path='/' element={<MainContainer/>}/>
+<Route path ='/favorite' element={<FavoriteDrawer items={cartFavorite}/>}/>
 </Routes>
 </BrowserRouter>
     </div>
+
   );
 }
 
--- src/components/BlockNewPlants/BlockNewPlants.jsx
+++ src/components/BlockNewPlants/BlockNewPlants.jsx
@@ -1,43 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/stylesBlockNewPlants.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './styles/stylesBlockNewPlants.css';
-import { FreeMode, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import FirstContainers from '../ContainersLists/FirstContainers';
+import axios from 'axios';
+import MyContext from '../../apjscontext';
+import FavoriteDrawer from './FavoriteDrawer';
+import { Link } from 'react-router-dom';
 
 
-const BlockNewPlants = ({itemsPlants}) => {
 
+
+
+
+
+
+
+const BlockNewPlants = () => {
+const [itemsFavorite, setItemsFavorite] = useState([])
+
+const [cartFavorite, setIsFavorite] = useState([]);
+
+
+
+
+
+
+useEffect(() =>{
+    
+async function fetchData(){
+  const itemsResponse = await axios.get('http://localhost:3005/FavoriteDrawer');
+  const cartResponse = await axios.get('http://localhost:3002/cart');
+
+setIsFavorite(cartResponse.data);
+setItemsFavorite(itemsResponse.data);
+
+}
+
+fetchData();
+  },[])
+
+  const onAddFavorite = (obj) => {
+if(cartFavorite.find((item) => Number(item.id) === Number(obj.id))) {
+// axios.delete(`http://localhost:3002/cart/${obj.id}`, obj);
+  setIsFavorite(prev =>prev.filter((item) => Number(item.id) !== Number(obj.id)))
+} else {
+ // axios.post('http://localhost:3002/cart',obj)
+  setIsFavorite(prev => [...prev, obj])
+}  
+ }
+   
+const onRemoveItem = (id, obj) =>{
+//axios.delete(`http://localhost:3002/cart/${id}`,obj);
+setIsFavorite(prev => prev.filter(item => item.id !== id));
+
+};
+
+
+const isItemAddToFavorite = (id) =>{
+ return cartFavorite.some((obj) => Number(obj.id) === Number(id))
+}
 
 
   return (
+<MyContext.Provider value={{isItemAddToFavorite, cartFavorite}}>
     <div className='blogNewPlants'>
- 
-<Swiper spaceBetween={20}
-      slidesPerView={3}
-      freeMode={true}
-      pagination={{clickable: true}}
-      modules={[Pagination, FreeMode]}>
- 
-      {itemsPlants.map((items,id) => (<div className='blogListPlants' key={id}>
-      <SwiperSlide ><FirstContainers
-        id={items.id}
-        title={items.title}
-        desc={items.desc}
-        image={items.image}
-        currency={items.currency}
-        price={items.price}
-        btn={items.btn}/>
-        
-              </SwiperSlide>
-      </div>))}
-
-      </Swiper>
+      <div className='blogListPlants'>
+        <Swiper spaceBetween={10}
+        pagination = {{clickable:true}}
+        modules={[Pagination]}
+        slidesPerView={3}
+        >
+       {itemsFavorite.map((items, id) =>(<SwiperSlide key={id}>
+       
+        <FirstContainers 
+  id={items.id}
+  btn = {items.btn} 
+  image = {items.image}
+  title ={items.title}
+  desc ={items.desc}
+  currency = {items.currency}
+  price = {items.price}
+  added = {(obj) =>onAddFavorite(obj) }
+  onRemove = {(obj) => onRemoveItem(obj)}
+        />   </SwiperSlide>))}     
+        </Swiper>
+        </div>  
       </div>
-  
+        
+      </MyContext.Provider>
+
   )
 }
 
-export default BlockNewPlants
+export default BlockNewPlants;
